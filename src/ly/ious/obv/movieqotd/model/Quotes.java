c0731f5b7688e79c8ce2759565919dff3ea0fbd7
jklett <jklett>
2009-02-04 02:02:20 +0000

    Fleshed out

--- src/ly/ious/obv/movieqotd/model/Quotes.java
+++ src/ly/ious/obv/movieqotd/model/Quotes.java
@@ -12,22 +12,29 @@
 
 package ly.ious.obv.movieqotd.model;
 
-import org.javaforge.sql.Table;
-import org.javaforge.sql.Column;
 import com.blipnetworks.sql.SQLBuilder;
+import com.blipnetworks.sql.SQLConstants;
+import com.blipnetworks.sql.SQLExec;
+import org.javaforge.sql.Column;
+import org.javaforge.sql.Table;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 
 /**
  * TODO
  *
  * @author Jared Klett
- * @version $Id: Quotes.java,v 1.2 2009/01/26 04:29:49 jklett Exp $
+ * @version $Id: Quotes.java,v 1.3 2009/02/04 02:02:20 jklett Exp $
  */
 
 public class Quotes {
 
 // CVS info ///////////////////////////////////////////////////////////////////
 
-    public static final String CVS_REV = "$Revision: 1.2 $";
+    public static final String CVS_REV = "$Revision: 1.3 $";
 
 // Table structure ////////////////////////////////////////////////////////////
 
@@ -46,11 +53,106 @@ public class Quotes {
 
 // Pre-made SQL queries ///////////////////////////////////////////////////////
 
+    public static final Object[] WHERE_GET_QUOTE_BY_MID = {
+            MID, SQLConstants.SQL_EQ, SQLConstants.SQL_QUES
+    };
+
     /** Generated SQL which loads all the rows in the table. */
     public static final String SQL_SELECT = SQLBuilder.buildSelect(
             new Table[] {TABLE},
             ALL_COLUMNS,
             null
     );
+
+    public static final String SQL_GET_QUOTE_BY_MID = SQLBuilder.buildSelect(
+            new Table[] {TABLE},
+            ALL_COLUMNS,
+            WHERE_GET_QUOTE_BY_MID
+            );
+
+// Instance variables /////////////////////////////////////////////////////////
+
+    private int qid;
+    private int mid;
+    private String quoteText;
+    private boolean used;
+    private Date usedDatestamp;
+
+// Constructor ////////////////////////////////////////////////////////////////
+
+    private Quotes() {
+        // should never be called outside
+    }
+
+// Class methods //////////////////////////////////////////////////////////////
+
+    public static Quotes getQuote(Connection connection, int mid) throws SQLException {
+        Quotes quote = null;
+        Object[] values = { mid };
+        ResultSet rs = SQLExec.doQuery(connection, SQL_GET_QUOTE_BY_MID, values);
+        if (rs.next())
+            quote = setParams(rs);
+        rs.close();
+        return quote;
+    }
+
+    /**
+     * Convenience method to create a new <code>Conversion</code> object and
+     * populate it with the fields from the passed result set.
+     *
+     * @param rs The <code>ResultSet</code> to get the values from.
+     * @return A newly populated <code>Conversion</code> object.
+     * @throws SQLException If a database access error occurs.
+     */
+    private static Quotes setParams(ResultSet rs) throws SQLException {
+        int i = 1;
+        Quotes quote = new Quotes();
+        quote.setQid(rs.getInt(i));
+        quote.setMid(rs.getInt(++i));
+        quote.setQuoteText(rs.getString(++i));
+        quote.setUsed(rs.getInt(++i) == 1);
+        quote.setUsedDatestamp(rs.getDate(++i));
+        return quote;
+    }
+
+    public int getQid() {
+        return qid;
+    }
+
+    public int getMid() {
+        return mid;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public Date getUsedDatestamp() {
+        return usedDatestamp;
+    }
+
+    public void setQid(int qid) {
+        this.qid = qid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public void setUsedDatestamp(Date usedDatestamp) {
+        this.usedDatestamp = usedDatestamp;
+    }
 
 } // class Quotes
