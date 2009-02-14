d65201b2e31e1b1813d5117ecf710c26d99095d6
jklett <jklett>
2009-02-14 17:57:22 +0000

    Changed to support trivia as well

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -1,30 +1,50 @@
-CREATE TABLE quotes (
-    qid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    mid INT NOT NULL,
+CREATE TABLE people (
+    person_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    twitter_name VARCHAR(255) NOT NULL
+) ENGINE=InnoDb;
+
+CREATE TABLE movie_quotes (
+    quote_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL,
     quote_text VARCHAR(140) NOT NULL,
     used TINYINT(1) NOT NULL DEFAULT 0,
     used_datestamp DATETIME
 ) ENGINE=InnoDb;
 
 CREATE TABLE movies (
-    mid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     movie_title VARCHAR(255) NOT NULL,
-    gid INT NOT NULL
+    genre_id INT NOT NULL
 ) ENGINE=InnoDb;
 
-CREATE TABLE genres (
-    gid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE movie_genres (
+    genre_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     genre_name VARCHAR(255) NOT NULL
 ) ENGINE=InnoDb;
 
-CREATE TABLE people (
-    pid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    twitter_name VARCHAR(255) NOT NULL
+CREATE TABLE movie_winners (
+    movie_win_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    person_id INT NOT NULL,
+    quote_id INT NOT NULL,
+    datestamp DATETIME NOT NULL
 ) ENGINE=InnoDb;
 
-CREATE TABLE winners (
-    wid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    pid INT NOT NULL,
-    qid INT NOT NULL,
+CREATE TABLE trivia_questions (
+    trivia_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_id INT NOT NULL,
+    question_text VARCHAR(255) NOT NULL,
+    used TINYINT(1) NOT NULL DEFAULT 0,
+    used_datestamp DATETIME
+) ENGINE=InnoDb;
+
+CREATE TABLE trivia_category (
+    category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+) ENGINE=InnoDb;
+
+CREATE TABLE trivia_winners (
+    trivia_win_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    person_id INT NOT NULL,
+    trivia_id INT NOT NULL,
     datestamp DATETIME NOT NULL
 ) ENGINE=InnoDb;
