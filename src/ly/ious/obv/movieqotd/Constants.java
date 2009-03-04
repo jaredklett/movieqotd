0d9c31e1f362ddcb00fa073e7bb3b957287bcaed
jklett <jklett>
2009-03-04 01:46:37 +0000

    cut n paste

---
+++ src/ly/ious/obv/movieqotd/Constants.java
@@ -0,0 +1,44 @@
+/*
+ * @(#)Constants.java
+ *
+ * Copyright (c) 2004-2008 by Obviously, Inc.
+ * 187 Lafayette St, 6th Floor
+ * New York, NY 10013
+ * All rights reserved.
+ *
+ * This software is the confidential and
+ * proprietary information of Obviously, Inc.
+ */
+
+package ly.ious.obv.movieqotd;
+
+/**
+ * TODO
+ *
+ * @author Jared Klett
+ * @version $Id: Constants.java,v 1.1 2009/03/04 01:46:37 jklett Exp $
+ */
+
+public class Constants {
+
+// CVS info ///////////////////////////////////////////////////////////////////
+
+    public static final String CVS_REV = "$Revision: 1.1 $";
+
+// Request keys ///////////////////////////////////////////////////////////////
+
+    public static final String PARAMETER_COMMAND = "command";
+    public static final String PARAMETER_OKAY = "okay.page";
+    public static final String PARAMETER_ERROR = "error.page";
+
+// Constructor ////////////////////////////////////////////////////////////////
+
+    private Constants() {
+        // will never be called outside the class
+    }
+
+// Initializer ////////////////////////////////////////////////////////////////
+
+    // none yet
+
+} // class Constants
